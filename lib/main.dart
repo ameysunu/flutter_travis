96a8110ef8b757e031122d9dbbdace0492ab3800
Amey Sunu <ameysunu00719@gmail.com>
2020-10-07 11:00:29 +0300

    alert added

--- lib/main.dart
+++ lib/main.dart
@@ -72,8 +72,30 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-Widget popUp(BuildContext context) {
-  return AlertDialog(
-    title: Text("Sup"),
+Future<void> popUp(BuildContext context) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('AlertDialog Title'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text('This is a demo alert dialog.'),
+              Text('Would you like to approve of this message?'),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Approve'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
   );
 }
